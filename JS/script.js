0328cfd8b89d907e643ad828924a6b07e9ee5020
Maksuda Akter <maksudaakterliza13@gmail.com>
2026-09-20 18:15:54 +0600

    nested object uploaded

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,61 @@
-// ------------------ object add ------------------
+// ================================== Object ===============================
 // let students = {
 //     name: "Imran",
 //     roll: 11,
 //     isPresent: true,
 //     adress: "Uttara",
 // }
-// students.email = "imran@gmail.com"
+// students.email = "imran@gmail.com"     // new object add 
 // console.log(students)
 // --------------------- this ----------------------
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 50,
-  fullName: function() {
-    return this.firstName + " " + this.lastName;  //object er vhitore access nite chaile this
-  }
-};
-console.log(person. fullName())
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;  //object er vhitore access nite chaile this
+//   }
+// };
+// console.log(person. fullName())
 
-const a = {fullName: person.firstName + " " + person.lastName  //object er baire access nite chaile 
+// const a = {fullName: person.firstName + " " + person.lastName  //object er baire access nite chaile 
 
+// }
+// console.log(a)
+
+    // ---------------------- delete -------------------
+//     let students = {
+//     name: "Imran",
+//     roll: 11,
+//     isPresent: true,
+//     adress: "Uttara",
+// }
+// delete students .adress  //ja delete korbo tar samne delete likhe dibo
+// console.log(students)
+//------------------------- check exist property ------------------------
+// let students = {
+//     name: "Imran",
+//     roll: 11,
+//     isPresent: true,
+//     adress: "Uttara",
+// }
+// let result =("roll" in students)   //roll students er vhitore ace kina check kora holo
+// console.log(result)
+// -------------------------- Nested Object -----------------------------
+let students = {
+    name: "Imran",
+    roll: 11,
+    isPresent: true,
+    adress: "Uttara",
+    hobby: {
+    hobby1: "Hiking",
+    hobby2: "Bird_Watching",
+    hobby3: "Photography",
+    hobby4: "Camping",
+    hobby5: "Gardening",
+}  
 }
-console.log(a)
+console.log(students.hobby)            //hobby access korte chaile        
+console.log(students.hobby.hobby1)     //hobby1 access korte chaile
+
     
