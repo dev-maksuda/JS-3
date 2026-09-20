6f4a1e752e38d996c8c3e915795334af19927b6b
Maksuda Akter <maksudaakterliza13@gmail.com>
2026-09-20 19:20:03 +0600

    Array uploaded

--- JS/script.js
+++ JS/script.js
@@ -58,4 +58,18 @@ let students = {
 console.log(students.hobby)            //hobby access korte chaile        
 console.log(students.hobby.hobby1)     //hobby1 access korte chaile
 
-    
+// ================================ Array ================================
+let studentName = "Imran"
+let studentsList = ["Imran", "Rahul", "Ahsan", "Tanvir", "Fahim", "Sajid", "Arif", "Nabil", "Rayhan", "Asif"];
+console.log(studentsList)               //studentslist dekhar jonno
+console.log(studentsList.length)        //koyta data ace seta dekhar jonno
+console.log(studentsList[5])            //6 no e kon data ace seta dekhar jonno
+console.log(studentsList[0])            // first e kon data ace seta dekhar jonno
+let lastIndex = studentsList.length-1
+console.log(studentsList[lastIndex])       // last e kon data ace seta dekhar jonno    
+studentsList[0]  = "Imran Hasan"   
+console.log(studentsList)                  //update korar jonno
+
+for(let i=0; i<studentsList.length; i++){
+    console.log(studentsList[i])                  // For Loop use kore first to last list ber kora
+}
